0dea79b67d75d34ecfd435a15491a5c51b3e516d
refinedcoding <63949022+refinedcoding@users.noreply.github.com>
2021-09-03 15:02:45 -0700

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,30 @@
+export MY_INSTALL_DIR=$HOME/.local
+mkdir -p $MY_INSTALL_DIR
+export PATH="$MY_INSTALL_DIR/bin:$PATH"
+
+git clone --recurse-submodules -b v1.38.0 https://github.com/grpc/grpc
+
+cd grpc
+mkdir -p cmake/build
+pushd cmake/build
+cmake -DgRPC_INSTALL=ON \
+      -DgRPC_BUILD_TESTS=OFF \
+      -DCMAKE_INSTALL_PREFIX=$MY_INSTALL_DIR \
+      ../..
+make -j
+make install
+popd
+mkdir -p third_party/abseil-cpp/cmake/build
+pushd third_party/abseil-cpp/cmake/build
+cmake -DCMAKE_INSTALL_PREFIX=$MY_INSTALL_DIR \
+      -DCMAKE_POSITION_INDEPENDENT_CODE=TRUE \
+      ../..
+make -j
+make install
+popd
+
+cd examples/cpp/helloworld
+mkdir -p cmake/build
+pushd cmake/build
+cmake -DCMAKE_PREFIX_PATH=$MY_INSTALL_DIR ../..
+make -j
