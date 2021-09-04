d1e902a8c9077d2ff690fd7760ec510620ff360e
refinedcoding <63949022+refinedcoding@users.noreply.github.com>
2021-09-03 17:02:36 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,30 +1,37 @@
-export MY_INSTALL_DIR=$HOME/.local
-mkdir -p $MY_INSTALL_DIR
-export PATH="$MY_INSTALL_DIR/bin:$PATH"
-
-git clone --recurse-submodules -b v1.38.0 https://github.com/grpc/grpc
-
-cd grpc
-mkdir -p cmake/build
-pushd cmake/build
-cmake -DgRPC_INSTALL=ON \
-      -DgRPC_BUILD_TESTS=OFF \
-      -DCMAKE_INSTALL_PREFIX=$MY_INSTALL_DIR \
-      ../..
-make -j
-make install
-popd
-mkdir -p third_party/abseil-cpp/cmake/build
-pushd third_party/abseil-cpp/cmake/build
-cmake -DCMAKE_INSTALL_PREFIX=$MY_INSTALL_DIR \
-      -DCMAKE_POSITION_INDEPENDENT_CODE=TRUE \
-      ../..
-make -j
-make install
-popd
-
-cd examples/cpp/helloworld
-mkdir -p cmake/build
-pushd cmake/build
-cmake -DCMAKE_PREFIX_PATH=$MY_INSTALL_DIR ../..
-make -j
+# 2 Environment
+export MY_INSTALL_DIR=$HOME/.local    
+mkdir -p $MY_INSTALL_DIR    
+export PATH="$MY_INSTALL_DIR/bin:$PATH"    
+    
+# 3 Download gRPC
+git clone --recurse-submodules -b v1.38.0 https://github.com/grpc/grpc    
+    
+# 4 Build gRPC
+cd grpc    
+mkdir -p cmake/build    
+pushd cmake/build    
+cmake -DgRPC_INSTALL=ON \    
+      -DBUILD_SHARED_LIBS=ON \    
+      -DCMAKE_BUILD_TYPE=Release \    
+      -DCMAKE_INSTALL_PREFIX=$MY_INSTALL_DIR \    
+      ../..    
+make -j4    
+make    
+make install    
+popd    
+mkdir -p third_party/abseil-cpp/cmake/build    
+pushd third_party/abseil-cpp/cmake/build    
+cmake -DCMAKE_INSTALL_PREFIX=$MY_INSTALL_DIR \    
+      -DCMAKE_POSITION_INDEPENDENT_CODE=TRUE \    
+      ../..    
+make -j4        
+make    
+make install    
+popd    
+    
+# 5 Build Example
+cd examples/cpp/helloworld    
+mkdir -p cmake/build    
+pushd cmake/build    
+cmake -DCMAKE_PREFIX_PATH=$MY_INSTALL_DIR ../..    
+make -j  
